529861424a8323216e27983258349079fdfa366a
isir10 <isir.malak@moringaschool.com>
2022-09-04 13:01:30 +0300

    completed assignment

--- main.js
+++ main.js
@@ -3,7 +3,28 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+let myModal = (document.querySelector("#modal").className = "hidden");
+let likeButtons = document.querySelectorAll(".like-glyph");
+likeButtons.forEach(function (likeButton) {
+	likeButton.addEventListener("click", function (e) {
+		mimicServerCall()
+			.then(function () {
+				if (likeButton.innerText === EMPTY_HEART) {
+					likeButton.innerText = FULL_HEART;
+					likeButton.className = "activated-heart";
+				} else {
+					likeButton.innerText = EMPTY_HEART;
+					likeButton.className = "";
+				}
+			})
+			.catch(function (error) {
+				myModal.className = "";
+				setTimeout(function () {
+					myModal.className = "hidden";
+				}, 5000);
+			});
+	});
+});
 
 
 
